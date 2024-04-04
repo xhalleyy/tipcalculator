86b8f4b57a3b3d561a75f3de6ac03df0993fd710
xhalleyy <xhalleyy@gmail.com>
2024-04-04 13:14:55 -0700

    fixed unnecessary symbols in input

--- src/app/components/CalculatorComponent.tsx
+++ src/app/components/CalculatorComponent.tsx
@@ -86,16 +86,13 @@ const CalculatorComponent = () => {
 
         if(value !== '' && /^\d*\.?\d+$/.test(value) && Number(value)){
             const customTipValue = Number(e.target.value);
+            
             const tipFraction = customTipValue / 100;
-
-            if(tipFraction !== 0.05 && tipFraction !== 0.1 && tipFraction !== 0.15 && tipFraction !== 0.25 && tipFraction !== 0.50){
                 setIsCustom(true);
                 setIsSelected(false);
                 setCustomAmount(customTipValue);
                 setTip(tipFraction);
-            }else {
-                setCustomAmount(undefined)
-            }
+
         }else {
             setIsCustom(false);
             setCustomAmount(undefined);
@@ -150,12 +147,12 @@ const CalculatorComponent = () => {
                     <div className="mt-8 lg:mt-10">
                         <p className="text-gray mb-3 text-sm"> Select Tip %</p>
                         <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 text-2xl text-offWhite">
-                            <button onClick={fivePercent} className={tip === 0.05 ? pickedTipClass : tipClass}>5%</button>
-                            <button onClick={tenPercent} className={tip === 0.10 ? pickedTipClass : tipClass}>10%</button>
-                            <button onClick={fifteenPercent} className={tip === 0.15 ? pickedTipClass : tipClass}>15%</button>
-                            <button onClick={quarterPercent} className={tip === 0.25 ? pickedTipClass : tipClass}>25%</button>
-                            <button onClick={fiftyPercent} className={tip === 0.50 ? pickedTipClass : tipClass}>50%</button>
-                            <input value={customAmount !== undefined ? customAmount : ''} onChange={customTip} type="number" placeholder="Custom" className={!isCustom ? "py-2 bg-offWhite text-gray rounded-lg focus:text-darkCyan text-center cursor-pointer" : "py-1.5 bg-offWhite text-gray rounded-lg focus:text-darkCyan text-center custom"} />
+                            <button onClick={fivePercent} className={tip === 0.05 && isSelected ? pickedTipClass : tipClass}>5%</button>
+                            <button onClick={tenPercent} className={tip === 0.10 && isSelected  ? pickedTipClass : tipClass}>10%</button>
+                            <button onClick={fifteenPercent} className={tip === 0.15 && isSelected  ? pickedTipClass : tipClass}>15%</button>
+                            <button onClick={quarterPercent} className={tip === 0.25 && isSelected  ? pickedTipClass : tipClass}>25%</button>
+                            <button onClick={fiftyPercent} className={tip === 0.50 && isSelected  ? pickedTipClass : tipClass}>50%</button>
+                            <input onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()} value={customAmount !== undefined ? customAmount : ''} onChange={customTip} type="number" placeholder="Custom" className={!isCustom ? "py-2 bg-offWhite text-gray rounded-lg focus:text-darkCyan text-center cursor-pointer" : "py-1.5 bg-offWhite text-gray rounded-lg focus:text-darkCyan text-center custom"} />
                         </div>
                     </div>
 
@@ -167,7 +164,7 @@ const CalculatorComponent = () => {
                         </div>
                         <div className={isSelected && isFilled && peopleCount === undefined || isCustom && isFilled && peopleCount === undefined ? "bg-offWhite px-4 py-1.5 rounded-md flex flex-row justify-between items-center zeroBorder" : "bg-offWhite px-4 py-1.5 rounded-md flex flex-row justify-between items-center border-2 border-gray/0 focus-within:border-cyan focus-within:caret-cyan"}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="13" height="16"><path fill="#9EBBBD" d="M9.573 7.729c.406 0 .784.07 1.126.209.342.14.639.33.881.569.232.227.438.503.614.82a5.1 5.1 0 01.407.949c.097.312.178.654.242 1.016.062.359.105.699.126 1.011.02.307.031.624.031.945 0 .836-.259 1.512-.768 2.01-.504.492-1.17.742-1.98.742H2.748c-.81 0-1.477-.25-1.98-.742C.259 14.76 0 14.084 0 13.248c0-.322.01-.64.032-.945.02-.312.063-.652.126-1.01.063-.363.144-.705.242-1.017.1-.323.238-.643.407-.948.176-.318.382-.594.613-.821.243-.238.54-.43.882-.57.342-.138.72-.208 1.125-.208.16 0 .313.067.61.265.183.123.397.264.636.421.204.134.48.259.822.372.333.11.671.167 1.005.167a3.19 3.19 0 001.006-.167c.341-.113.618-.238.822-.372l.636-.42c.296-.2.45-.266.61-.266zM6.598 0C7.63 0 8.522.38 9.252 1.129s1.1 1.666 1.1 2.724c0 1.06-.37 1.976-1.1 2.725-.73.75-1.623 1.13-2.654 1.13-1.03 0-1.924-.38-2.653-1.13-.73-.749-1.1-1.666-1.1-2.725 0-1.058.37-1.975 1.1-2.724C4.675.379 5.567 0 6.598 0z" /></svg>
-                            <input value={peopleCount ?? ''} onChange={handlePeople} className="!w-52 lg:!w-auto bg-offWhite text-2xl text-end border-0 border:border-0 text-darkCyan" min='0' type="number" placeholder="0" />
+                            <input onKeyDown={(evt) => ["e", "E", "+", "-", "."].includes(evt.key) && evt.preventDefault()} value={peopleCount ?? ''} onChange={handlePeople} className="!w-52 lg:!w-auto bg-offWhite text-2xl text-end border-0 border:border-0 text-darkCyan" min='0' type="number" placeholder="0" />
                         </div>
                     </div>
                 </div>
